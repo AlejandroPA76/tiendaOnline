5e57a264c7febc95b28dbe7ac65302657f05d371
CristianCamach0 <cristiancamacho_99@hotmail.com>
2021-10-22 08:50:20 -0500

    Cambios

---
+++ resources/views/login.blade.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>LOGIN</title>
+    <link rel="resources/css" href="login.css">
+</head>
+<body>
+    <form action="login.php" method="POST" class="form-box">
+    <h1 class="form-title">Iniciar sesión</h1>
+
+    <p class="form-text"><label form="nickname">Username</label>
+    <input type="text" name="username""</p>
+
+    <p class="form-text"><lavel for="password">Password</lavel>
+    <input type="text" name="password"></p>
+    
+    <button type="submit" name="login">Login</button>
+    </form>
+</body>
+</html>
