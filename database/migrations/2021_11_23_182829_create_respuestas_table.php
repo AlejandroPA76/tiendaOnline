b0cb8bd751eb59feb6b781cfde8d538a3e923c42
AlejandroPA76 <alejandrokeene@gmail.com>
2021-11-23 12:49:56 -0600

    agregue tabla

--- database/migrations/2021_11_23_182829_create_respuestas_table.php
+++ database/migrations/2021_11_23_182829_create_respuestas_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateComentariosTable extends Migration
+class CreateRespuestasTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,17 +13,17 @@ class CreateComentariosTable extends Migration
      */
     public function up()
     {
-        Schema::create('comentarios', function (Blueprint $table) {
+        Schema::create('respuestas', function (Blueprint $table) {
             $table->id();
+            $table->string('respuesta');
             $table->timestamps();
-            $table->text('comentario');
             
-            $table->foreignId('user_id')
-            ->constrained('users') 
+            $table->foreignId('comentarios_id')
+            ->constrained('comentarios')
             ->onDelete('cascade');
-            
-            $table->foreignId('productos_id')
-            ->constrained('productos')
+
+            $table->foreignId('user_id')
+            ->constrained('users')
             ->onDelete('cascade');
         });
     }
@@ -35,6 +35,6 @@ class CreateComentariosTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('comentarios');
+        Schema::dropIfExists('respuestas');
     }
 }
